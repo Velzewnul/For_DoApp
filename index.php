a37f3516f538d6cab0329f208f8359484b08f5f7
Velzewnul <vitbulgakov@gmail.com>
2023-03-24 20:43:22 +0700

    Создал шаблон для первого пользователя, сделал форму и заготовку под php-код, который ещё буду прикручивать в index

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
 <?php
+session_start();
+
+// проверяем, отправлены ли данные из формы
+if(isset($_POST["login"])) {
+    // создаём объект пользователя
+    $user = new User();
+
+}
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+<!-- проверяем, записана ли ошибка и отображаем -->
+<?php if(isset($error)) { ?>
+    <p><?= $error; ?></p>
+<?php } ?>
+<form method="post" action="">
+    <label>Username:</label>
+    <input type="text" name="username" required><br><br>
+    <label>Password:</label>
+    <input type="password" name="password" required><br><br>
+    <input type="submit" name="login" value="Login">
+</form>
+</body>
+</html>
